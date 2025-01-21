4adf4f6da8caeede68aca617950780e8aa0a3914
Thevenin-Jonathan <warzonefury@hotmail.fr>
2025-01-21 21:19:12 +0100

    Supprime le renommage des pistes car inutile

--- encoding.py
+++ encoding.py
@@ -53,7 +53,6 @@ def lancer_encodage(dossier, fichier, preset, file_encodage):
         copier_fichier_dossier_encodage_manuel(input_path)
         return
 
-    # Supposons que pistes_audio et sous_titres sont déjà définis
     options_audio = f'--audio={",".join(map(str, pistes_audio))}'
     options_sous_titres = f'--subtitle={",".join(map(str, sous_titres))}'
     options_burn = (
@@ -61,20 +60,6 @@ def lancer_encodage(dossier, fichier, preset, file_encodage):
         if sous_titres_burn is not None
         else ""
     )
-
-    # Générer autant de chaînes vides que le nombre de pistes audio
-    aname_values = ",".join(['""' for _ in pistes_audio])
-    options_aname = f"--aname={aname_values}"
-
-    # Calculer le nombre de sous-titres à inclure dans --subname
-    if sous_titres_burn is not None:
-        num_sous_titres = len(sous_titres) - 1
-    else:
-        num_sous_titres = len(sous_titres)
-
-    # Générer autant de chaînes vides que le nombre de sous-titres (ajusté si nécessaire)
-    subname_values = ",".join(['""' for _ in range(num_sous_titres)])
-    options_subname = f"--subname={subname_values}"
 
     commande = [
         "HandBrakeCLI",
@@ -89,8 +74,6 @@ def lancer_encodage(dossier, fichier, preset, file_encodage):
         options_audio,
         options_sous_titres,
         options_burn,
-        options_aname,
-        options_subname,
     ]
 
     print(f"{horodatage()} 🔧 Commande d'encodage : {' '.join(commande)}")
