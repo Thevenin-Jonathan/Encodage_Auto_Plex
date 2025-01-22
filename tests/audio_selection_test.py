ab55d42bc34836475137599e34320c176c2d6595
Thevenin-Jonathan <warzonefury@hotmail.fr>
2025-01-23 00:35:06 +0100

    Amélioration des tests unitaires audio

--- tests/audio_selection_test.py
+++ tests/audio_selection_test.py
@@ -10,31 +10,99 @@ from audio_selection import selectionner_pistes_audio
 
 class TestAudioSelection(unittest.TestCase):
     def setUp(self):
-        self.info_pistes = {
+        self.info_pistes1 = {
             "TitleList": [
                 {
                     "AudioList": [
-                        {"TrackNumber": 1, "LanguageCode": "fra", "Name": "French"},
-                        {"TrackNumber": 2, "LanguageCode": "eng", "Name": "English"},
+                        {"TrackNumber": 1, "LanguageCode": "fra", "Name": "Full"},
+                        {"TrackNumber": 2, "LanguageCode": "eng", "Name": "Full"},
+                    ]
+                }
+            ]
+        }
+
+        self.info_pistes2 = {
+            "TitleList": [
+                {
+                    "AudioList": [
+                        {"TrackNumber": 1, "LanguageCode": "fra", "Name": ""},
+                        {"TrackNumber": 2, "LanguageCode": "fra", "Name": "VFQ"},
+                        {"TrackNumber": 3, "LanguageCode": "fra", "Name": "VF-ad"},
+                        {"TrackNumber": 4, "LanguageCode": "fra", "Name": "AD"},
+                        {
+                            "TrackNumber": 5,
+                            "LanguageCode": "fra",
+                            "Name": "audiodescription",
+                        },
+                        {"TrackNumber": 6, "LanguageCode": "fra", "Name": "quebec"},
+                        {"TrackNumber": 7, "LanguageCode": "fra", "Name": "canada"},
+                        {
+                            "TrackNumber": 8,
+                            "LanguageCode": "fra",
+                            "Name": "descriptive audio",
+                        },
+                    ]
+                }
+            ]
+        }
+
+        self.info_pistes3 = {
+            "TitleList": [
+                {
+                    "AudioList": [
+                        {
+                            "TrackNumber": 2,
+                        },
+                        {"TrackNumber": 1, "LanguageCode": "fra"},
                     ]
                 }
             ]
         }
 
     def test_selectionner_pistes_audio_dessins_animes(self):
-        preset = "Dessins animés FR 1000kbps"
-        result = selectionner_pistes_audio(self.info_pistes, preset)
+        preset = "1080p HD-Light 1500kbps"
+
+        # Test avec info_pistes1
+        result = selectionner_pistes_audio(self.info_pistes1, preset)
+        self.assertEqual(result, [1])
+
+        # Test avec info_pistes2
+        result = selectionner_pistes_audio(self.info_pistes2, preset)
+        self.assertEqual(result, None)
+
+        # Test avec info_pistes3
+        result = selectionner_pistes_audio(self.info_pistes3, preset)
         self.assertEqual(result, [1])
 
     def test_selectionner_pistes_audio_mangas_multi(self):
         preset = "Mangas MULTI 1000kbps"
-        result = selectionner_pistes_audio(self.info_pistes, preset)
+
+        # Test avec info_pistes1
+        result = selectionner_pistes_audio(self.info_pistes1, preset)
+        self.assertEqual(result, [1, 2])
+
+        # Test avec info_pistes2
+        result = selectionner_pistes_audio(self.info_pistes2, preset)
+        self.assertEqual(result, [1, 2, 3, 4, 5, 6, 7, 8])
+
+        # Test avec info_pistes3
+        result = selectionner_pistes_audio(self.info_pistes3, preset)
         self.assertEqual(result, [1, 2])
 
     def test_selectionner_pistes_audio_mangas_vo(self):
         preset = "Mangas VO 1000kbps"
-        result = selectionner_pistes_audio(self.info_pistes, preset)
+
+        # Test avec info_pistes1
+        result = selectionner_pistes_audio(self.info_pistes1, preset)
         self.assertEqual(result, [1, 2])
+
+        # Test avec info_pistes2
+        result = selectionner_pistes_audio(self.info_pistes2, preset)
+        self.assertEqual(result, None)
+
+        # Test avec info_pistes3
+        result = selectionner_pistes_audio(self.info_pistes3, preset)
+        self.assertEqual(result, [2, 1])
 
 
 if __name__ == "__main__":
