7e094b3522077e3eb8e6ec96b0e10882c1fef6f2
Thevenin-Jonathan <warzonefury@hotmail.fr>
2025-02-28 01:54:17 +0100

    Modification du chemin de base du dossier de logs

--- logger.py
+++ logger.py
@@ -2,9 +2,18 @@ import os
 import logging
 from logging.handlers import RotatingFileHandler
 import datetime
+import sys
+
+# Définir le chemin de base en fonction de l'exécution en tant que script ou exécutable
+if getattr(sys, "frozen", False):
+    # Mode exécutable - utiliser le dossier où se trouve l'exécutable
+    BASE_PATH = os.path.dirname(sys.executable)
+else:
+    # Mode développement - utiliser le dossier du script
+    BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 
 # Créer le dossier logs s'il n'existe pas
-logs_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "logs")
+logs_dir = os.path.join(BASE_PATH, "logs")
 os.makedirs(logs_dir, exist_ok=True)
 
 
